8b4bb7d65644db4e44d6b9604c06d6399e9148cb
Adam Keenan <adam.g.keenan@gmail.com>
2021-06-06 23:12:42 -0400

    Set target to serverless for netlify

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+  target: 'serverless',
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
