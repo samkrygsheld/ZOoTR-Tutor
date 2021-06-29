976cf4094fd34ccc5f89ded79d213759f68cebe5
Adam Keenan <adam.g.keenan@gmail.com>
2021-06-29 17:33:42 -0400

    Created interfaces to restrict the region object

--- public/js/logic.ts
+++ public/js/logic.ts
@@ -37,40 +37,50 @@ const inventory = {
 const roots: string[] = [childSpawn, adultSpawn, 'prelude_warp', 'minuet_warp', 'bolero_warp', 'serenade_warp', 'nocturne_warp', 'requiem_warp'];
 
 /* Helpers */
-function isChild() {
+function isChild(): boolean {
   return age === 'child';
 }
 
-function isAdult() {
+function isAdult(): boolean {
   return age === 'adult';
 }
 
-function canLeaveForest() {
+function canLeaveForest(): boolean {
   return open_forest || isAdult() || checkStatuses.queenGohma;
 }
 
-function hasExplosives() {
-  return inventory.bombs || (bombchus_in_logic && inventory.bombchus);
+function hasExplosives(): boolean {
+  return !!(inventory.bombs || (bombchus_in_logic && inventory.bombchus));
 }
 
-function canChildAttack() {
-  return inventory.slingshot || inventory.boomerang || inventory.sticks || inventory.swordKokiri || hasExplosives() || (inventory.magic && inventory.dins);
+function canChildAttack(): boolean {
+  return !!(inventory.slingshot || inventory.boomerang || inventory.sticks || inventory.swordKokiri || hasExplosives() || (inventory.magic && inventory.dins));
 }
 
-function canPlantBugs() {
-  return inventory.bottle;
+function canPlantBugs(): boolean {
+  return !!inventory.bottle;
 }
 
-function canOpenStormGrotto() {
-  return (inventory.ocarina && inventory.storms) && (inventory.agony || logic_grottos_without_agony);
+function canOpenStormGrotto(): boolean {
+  return !!((inventory.ocarina && inventory.storms) && (inventory.agony || logic_grottos_without_agony));
 }
 
-function canStunDeku() {
-  return isAdult() || (inventory.shieldDeku || inventory.slingshot || inventory.boomerang || inventory.sticks || inventory.swordKokiri || hasExplosives() || (inventory.magic && inventory.dins) || inventory.nuts);
+function canStunDeku(): boolean {
+  return !!(isAdult() || (inventory.shieldDeku || inventory.slingshot || inventory.boomerang || inventory.sticks || inventory.swordKokiri || hasExplosives() || (inventory.magic && inventory.dins) || inventory.nuts));
 }
 
 /* Regions */
-const regions = [
+interface RuleObj {
+  [name: string]: () => boolean;
+}
+interface Region {
+  regionName: string;
+  scene: string;
+  hint?: string;
+  locations?: RuleObj;
+  exits: RuleObj;
+}
+const regions: Region[] = [
   {
     regionName: 'Kokiri Forest',
     scene: 'Kokiri Forest',
@@ -78,8 +88,8 @@ const regions = [
     locations: {
       'KF Kokiri Sword Chest': () => isChild(),
       'KF GS Know It All House': () => isChild() && canChildAttack(),
-      'KF GS Bean Patch': () => isChild() && inventory.bottle && canChildAttack(),
-      'KF GS House of Twins': () => isAdult() && inventory.shot, // (logic_adult_kokiri_gs && can_use(Hover_Boots)))
+      'KF GS Bean Patch': () => !!(isChild() && inventory.bottle && canChildAttack()),
+      'KF GS House of Twins': () => !!(isAdult() && inventory.shot), // (logic_adult_kokiri_gs && can_use(Hover_Boots)))
     },
     exits: {
       'KF Links House': () => true,
@@ -87,7 +97,7 @@ const regions = [
       'KF Outside Deku Tree': () => isAdult() || open_forest || (inventory.swordKokiri && inventory.shieldDeku),
       'Lost Woods': () => true,
       'LW Bridge From Forest': () => canLeaveForest(),
-      'KF Storms Grotto': () => canOpenStormGrotto,
+      'KF Storms Grotto': () => canOpenStormGrotto(),
     },
   },
   {
@@ -132,9 +142,9 @@ const regions = [
     scene: 'Lost Woods',
     hint: 'the Lost Woods',
     locations: {
-      'LW Skull Kid': () => isChild() && (inventory.ocarina && inventory.sarias),
-      'LW Ocarina Memory Game': () => isChild() && inventory.ocarina,
-      'LW Target in Woods': () => isChild() && inventory.slingshot,
+      'LW Skull Kid': () => isChild() && !!(inventory.ocarina && inventory.sarias),
+      'LW Ocarina Memory Game': () => isChild() && !!inventory.ocarina,
+      'LW Target in Woods': () => isChild() && !!inventory.slingshot,
       'LW Deku Scrub Near Bridge': () => isChild() && canStunDeku(),
       'LW GS Bean Patch Near Bridge': () => canPlantBugs() || canChildAttack(),
       // 'Bug Shrub': () => 'is_child and can_cut_shrubs and has_bottle',
@@ -142,15 +152,13 @@ const regions = [
     exits: {
       'LW Forest Exit': () => true,
       'GC Woods Warp': () => true, // COME BACK
-      'LW Bridge': () => isAdult() && (inventory.bootshover || inventory.shot === 2), //" is_adult and (can_use(Hover_Boots) or can_use(Longshot) or here(can_plant_bean) or logic_lost_woods_bridge)", COME BACK
-      'Zora River': () => canLeaveForest() && (inventory.scale || (isAdult() && inventory.bootsiron)),
+      'LW Bridge': () => isAdult() && (!!inventory.bootshover || inventory.shot === 2), //" is_adult and (can_use(Hover_Boots) or can_use(Longshot) or here(can_plant_bean) or logic_lost_woods_bridge)", COME BACK
+      'Zora River': () => canLeaveForest() && !!(inventory.scale || (isAdult() && inventory.bootsiron)),
       'LW Beyond Mido': () => isChild() || logic_mido_backflip || (inventory.ocarina && inventory.sarias),
-      'LW Near Shortcuts Grotto': 'here(can_blast_or_smash)', // COME BACK - here()
+      // 'LW Near Shortcuts Grotto': 'here(can_blast_or_smash)', // COME BACK - here()
     },
   },
 ];
-
-
 
 
 function accessHyruleField() {
